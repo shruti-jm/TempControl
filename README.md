ed580ae5da328a77082dde0daaf3569539526320
Shruti Maliakal <shrutijose@hotmail.com>
2018-05-30 19:20:22 -0700

    Temporary deletion

--- README.md
+++
@@ -1,2 +0,0 @@
-# TempControl
-Gym environment for testing RL algorithms for temperature control
--- Temp_Ctrl/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from Temp_Ctrl.envs.VacuumCan import VacCan
-from Temp_Ctrl.envs.VacuumCan_extrahard_env import VacCanExtraHardEnv
